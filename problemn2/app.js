4668a2d294c6d4a9cdd3fbe5908716c311c794a9
ftm-khamse <khamsehfatemeh347@gmail.com>
2025-09-07 23:01:36 -0700

    problem n2 solved

---
+++ problemn2/app.js
@@ -0,0 +1,12 @@
+let s1= 'cs';
+let s2 = 'disoc';
+
+if(s2.includes(s1)){console.log(true);}
+else{
+    let x = s2.split('');
+    x.reverse();
+    let result = x.join('')
+    if(result.includes(s1)){console.log(true);
+    }else{console.log(false);
+    }  
+}
