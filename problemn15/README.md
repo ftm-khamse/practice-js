7aa6276323345839f4ed37bdb76aa5fc68ea5f3c
ftm-khamse <khamsehfatemeh347@gmail.com>
2025-09-12 03:15:26 -0700

    problem 15 solved

---
+++ problemn15/README.md
@@ -0,0 +1,3 @@
+# problem
+
+This program takes an English alphabet and informs the user if the given alphabet is a vowel or a constant.
---
+++ problemn15/app.js
@@ -0,0 +1,6 @@
+function vowelConsonant(alpha){
+    const vowel = ['a','e','i','o','u'];
+    vowel.includes(alpha) ? console.log(`${alpha} is a vowel`) :console.log(`${alpha} is a constant`); 
+
+}
+vowelConsonant('i');
