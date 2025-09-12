33af92e6979566d4d324e9277b3ab31e62b2287b
ftm-khamse <khamsehfatemeh347@gmail.com>
2025-09-12 04:30:49 -0700

    problem 17 solved

--- problemn16/README.md
+++ problemn16/README.md
@@ -1,3 +1,7 @@
 # problem
 
 This program takes lengths of three sides and displays area of triangle.
+
+# problem 17
+
+This program generates a random number between minimum and maximum value.
--- problemn16/app.js
+++ problemn16/app.js
@@ -3,4 +3,10 @@ function area(a,b,c){
     console.log(Math.sqrt(p*(p-a)*(p-b)*(p-c)));
 }
 
-area(3,4,5);
+// area(3,4,5);
+
+function randomNumber(max,min){
+    console.log(Math.floor(Math.random() * (max-min)) + min);
+    
+}
+randomNumber(14,2)
